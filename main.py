336f4c615588ceb7aaf6737ab187b2b6e96a1f44
andre kes <andre_kestering@yahoo.com.br>
2020-08-12 19:15:20 -0300

    aula 01

---
+++ main.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+
+
+   # exercicios
+
+    valor_1 = input("digite o valor 1:")
+    valor_2 = input("digite o valor 2:")
+    mensagem = 'os valores são iguais.'
+    if valor_1 == valor_2:
+        print(mensagem)
+    if valor_1 > valor_2:
+        print(valor_1)
+    if valor_2 > valor_1:
+        print(valor_2)
+
+
+
